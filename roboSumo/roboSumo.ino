7937e2e5f4ce326f01f295776809aea6d19ade91
Rayssa Buarque <102766434+RayssaBuarque@users.noreply.github.com>
2023-09-13 11:54:27 -0300

    Update and rename roboSumox.ino to roboSumo.ino

--- roboSumo/roboSumo.ino
+++ roboSumo/roboSumo.ino
@@ -3,11 +3,9 @@
 // Rayssa Buarque
 
 //porta digital do motor
-#define motorA 12 
-#define motorB 8 
+#define motor 12 
 
 int valor_Bluetooth = 0; // Dados bluetooth
-int valor_entrada = 0; //valor após mapeamento
 
 void setup()
 {
@@ -15,18 +13,19 @@ void setup()
   Serial.begin(9600); 
 
   //configurando porta dos motores
-  pinMode(motorA, OUTPUT);
-  pinMode(motorB, OUTPUT); 
+  pinMode(motor, OUTPUT);
   
 }
 
 void loop()
 {
+  //caso alguma informação na porta serial
   if(Serial.available()){
+    //
     valor_Bluetooth = Serial.read();
     Serial.println(valor_Bluetooth);
     
-    analogWrite(motorA, valor_Bluetooth);
+    analogWrite(motor, valor_Bluetooth);
    
      delay(10);
   }
